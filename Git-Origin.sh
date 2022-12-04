a88a1913d0326448de6bc1b2f3a42b5919040d56
Micah Longmire <mlmicahlongmire@gmail.com>
2022-12-04 14:56:23 -0700

    Stuff

---
+++ Git-Origin.sh
@@ -0,0 +1,6 @@
+# Remove all existing values for remote.origin.url
+git config --unset-all remote.origin.url
+
+# Set a new value for remote.origin.url
+git config remote.origin.url "git@github.com:bobbyhiddn/Nexus-Upload-Application.git"
+
